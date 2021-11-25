f74a20de531507ee21103626c9763018365b4ab4
Victor <victor.rinnegard@hotmail.com>
2021-11-25 23:54:49 +0000

    Added some more content to footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,9 @@
 <footer>
     <p>&#169; rinnegard</p>
+    <p>
+        <a href="https://github.com/rinnegard/mvc-proj">Github</a>
+    </p>
+    <p>
+        <a href="https://dbwebb.se/kurser/mvc-v1">Kurs</a>
+    </p>
 </footer>
