224bf8a8efab9519553cee89e35d833f3af2aead
Victor <victor.rinnegard@hotmail.com>
2021-11-26 00:00:21 +0000

    Updated header style

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,16 +1,18 @@
-<header>
-    <h1>Yatzy</h1>
-    <nav>
-        <ul>
-            <li>
-                <a href="{{route("hello")}}">Home</a>
-            </li>
-            <li>
-                <a href="{{route("yatzy")}}">Yatzy</a>
-            </li>
-            <li>
-                <a href="{{route("highscore")}}">Highscore</a>
-            </li>
-        </ul>
-    </nav>
-</header>
+<div class="header">
+    <header>
+        <h1>Yatzy</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="{{route("hello")}}">Home</a>
+                </li>
+                <li>
+                    <a href="{{route("yatzy")}}">Yatzy</a>
+                </li>
+                <li>
+                    <a href="{{route("highscore")}}">Highscore</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</div>
